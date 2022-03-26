20af1c5e7a0bfb49d4925b5396c26b96a1da2271
Zenitsu-ID <zenitsuxd5@gmail.com>
2022-03-26 11:44:11 +0700

    lavender

--- target.sh
+++ target.sh
@@ -11,9 +11,7 @@ echo "Downloading Sources Target . . ."
 
 # Kernel Sources
 git clone --depth=1 https://github.com/ZenitsuID/kernel_xiaomi_lavender -b oldcam-hmp $CIRRUS_WORKING_DIR/$DEVICE_CODENAME
-# Toolchain 
-git clone --depth 1 https://github.com/NFS-projects/gcc-arm -b 12.x $CIRRUS_WORKING_DIR/GCC32
-git clone --depth 1 https://github.com/NFS-projects/gcc-arm64 -b 12.x $CIRRUS_WORKING_DIR/GCC64
-git clone --depth 1 https://github.com/NFS-projects/gcc-arm -b 11.x $CIRRUS_WORKING_DIR/GCC-11-32
-git clone --depth 1 https://github.com/NFS-projects/gcc-arm64 -b 11.x $CIRRUS_WORKING_DIR/GCC-11-64
-git clone --depth 1 https://github.com/NFS-projects/NFS-clang -b main $CIRRUS_WORKING_DIR/CLANG
+# Toolchain
+git clone --depth=1 https://github.com/sohamxda7/llvm-stable clang $CIRRUS_WORKING_DIR/CLANG
+git clone https://github.com/sohamxda7/llvm-stable -b gcc64 --depth=1 gcc $CIRRUS_WORKING_DIR/GCC-11-64
+git clone https://github.com/sohamxda7/llvm-stable -b gcc32 --depth=1 gcc32 $CIRRUS_WORKING_DIR/GCC-11-32
