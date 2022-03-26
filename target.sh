2b79aed89a419d91039b770625cd3f3a46b74989
Zenitsu-ID <zenitsuxd5@gmail.com>
2022-03-26 14:05:24 +0700

    lavender

--- target.sh
+++ target.sh
@@ -10,8 +10,8 @@
 echo "Downloading Sources Target . . ."
 
 # Kernel Sources
-git clone --depth=1 https://github.com/wHo-EM-i/kernel_xiaomi_lavender -b hmp $CIRRUS_WORKING_DIR/$DEVICE_CODENAME
+git clone --depth=1 https://github.com/ZenitsuID/kernel_xiaomi_lavender -b caf-hmp $CIRRUS_WORKING_DIR/$DEVICE_CODENAME
 # Toolchain
 git clone --depth=1 https://github.com/mvaisakh/gcc-arm64 -b gcc-master $CIRRUS_WORKING_DIR/GCC64
 git clone --depth=1 https://github.com/mvaisakh/gcc-arm -b gcc-master $CIRRUS_WORKING_DIR/GCC32
-# git clone --depth=1 https://gitlab.com/ImSpiDy/azure-clang.git -b clang-14 $CIRRUS_WORKING_DIR/CLANG
+git clone --depth=1 https://gitlab.com/ElectroPerf/atom-x-clang.git -b atom-15 $CIRRUS_WORKING_DIR/CLANG
