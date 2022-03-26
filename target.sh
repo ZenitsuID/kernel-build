54c3176fb9d3dce01f674461e907feacb172ee70
Zenitsu-ID <zenitsuxd5@gmail.com>
2022-03-26 13:57:43 +0700

    lavender

--- target.sh
+++ target.sh
@@ -14,4 +14,4 @@ git clone --depth=1 https://github.com/wHo-EM-i/kernel_xiaomi_lavender -b hmp $C
 # Toolchain
 git clone --depth=1 https://github.com/mvaisakh/gcc-arm64 -b gcc-master $CIRRUS_WORKING_DIR/GCC64
 git clone --depth=1 https://github.com/mvaisakh/gcc-arm -b gcc-master $CIRRUS_WORKING_DIR/GCC32
-git clone --depth=1 https://gitlab.com/ImSpiDy/azure-clang.git -b clang-14 $CIRRUS_WORKING_DIR/CLANG
+# git clone --depth=1 https://gitlab.com/ImSpiDy/azure-clang.git -b clang-14 $CIRRUS_WORKING_DIR/CLANG
