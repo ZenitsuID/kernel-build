292e674d9a5bc84dd283cb210e2c1c98fdd9042a
Zenitsu-ID <zenitsuxd5@gmail.com>
2022-03-26 12:53:36 +0700

    lavender

--- target.sh
+++ target.sh
@@ -12,6 +12,6 @@ echo "Downloading Sources Target . . ."
 # Kernel Sources
 git clone --depth=1 https://github.com/ZenitsuID/android_kernel_xiaomi_lavender-LTO -b hmp-main $CIRRUS_WORKING_DIR/$DEVICE_CODENAME
 # Toolchain
-git clone --depth=1 https://github.com/arter97/arm64-gcc -b master $CIRRUS_WORKING_DIR/GCC64
-git clone --depth=1 https://github.com/arter97/arm32-gcc -b master $CIRRUS_WORKING_DIR/GCC32
+git clone --depth=1 https://github.com/mvaisakh/gcc-arm64 -b gcc-master $CIRRUS_WORKING_DIR/GCC64
+git clone --depth=1 https://github.com/mvaisakh/gcc-arm -b gcc-master $CIRRUS_WORKING_DIR/GCC32
 # git clone --depth=1 https://github.com/kdrag0n/proton-clang -b master $CIRRUS_WORKING_DIR/CLANG
