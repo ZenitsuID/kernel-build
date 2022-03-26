be61ee35e4875f6864ebbcc5118ba7f81f0372ea
Zenitsu-ID <zenitsuxd5@gmail.com>
2022-03-26 10:40:04 +0700

    poki

--- target.sh
+++ target.sh
@@ -11,7 +11,7 @@ echo "Downloading Sources Target . . ."
 
 # Kernel Sources
 git clone --depth=1 https://github.com/ZenitsuID/kernel_xiaomi_lavender -b hmp $CIRRUS_WORKING_DIR/$DEVICE_CODENAME
-# Toolchain
+# Toolchain 
 git clone --depth=1 https://github.com/sohamxda7/llvm-stable -b gcc32 $CIRRUS_WORKING_DIR/GCC32
 git clone --depth=1 https://github.com/sohamxda7/llvm-stable -b gcc64 $CIRRUS_WORKING_DIR/GCC64
 git clone --depth=1 https://gitlab.com/crdroidandroid/android_prebuilts_clang_host_linux-x86_clang-r433403.git -b 11.0 $CIRRUS_WORKING_DIR/CLANG
