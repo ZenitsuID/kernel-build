dc43e12c989ddef5a43109fcd33b992aa6dc4639
Zen-Prjkt <zenitsuprjkt@gmail.com>
2022-03-29 23:10:01 +0700

    .

--- download.sh
+++ download.sh
@@ -3,7 +3,7 @@
 echo "Downloading few Dependecies . . ."
 
 # Kernel Sources
-git clone --depth=1 https://github.com/ZenitsuPrjkt/kernel_xiaomi_lavender -b caf-hmp $CIRRUS_WORKING_DIR/$DEVICE_CODENAME
+git clone --depth=1 https://github.com/ZenitsuID/kernel_xiaomi_lavender -b caf-hmp $CIRRUS_WORKING_DIR/$DEVICE_CODENAME
 # Toolchain 
 git clone --depth=1 https://github.com/sohamxda7/llvm-stable -b gcc64 $CIRRUS_WORKING_DIR/GCC64
 git clone --depth=1 https://github.com/sohamxda7/llvm-stable -b gcc32 $CIRRUS_WORKING_DIR/GCC32
