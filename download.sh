d62a916c6b945daf4848df84c40848ef97cec62f
Zen-Prjkt <zenitsuprjkt@gmail.com>
2022-04-02 22:49:24 +0700

    .

--- download.sh
+++ download.sh
@@ -3,8 +3,8 @@
 echo "Downloading few Dependecies . . ."
 
 # Kernel Sources
-git clone --depth=1 https://github.com/ZenitsuID/kernel_xiaomi_lavender -b newcam-hmp $CIRRUS_WORKING_DIR/$DEVICE_CODENAME
+git clone --depth=1 https://github.com/ZenitsuID/kernel_xiaomi_lavender -b oldcam-hmp $CIRRUS_WORKING_DIR/$DEVICE_CODENAME
 # Toolchain 
-git clone --depth=1 https://github.com/mvaisakh/gcc-arm64.git -b gcc-master $CIRRUS_WORKING_DIR/GCC64
-git clone --depth=1 https://github.com/mvaisakh/gcc-arm.git -b gcc-master $CIRRUS_WORKING_DIR/GCC32
+git clone --depth=1 https://github.com/mvaisakh/gcc-arm64 -b gcc-master $CIRRUS_WORKING_DIR/GCC64
+git clone --depth=1 https://github.com/mvaisakh/gcc-arm -b gcc-master $CIRRUS_WORKING_DIR/GCC32
 #git clone --depth=1 https://github.com/kdrag0n/proton-clang -b master $CIRRUS_WORKING_DIR/CLANG
