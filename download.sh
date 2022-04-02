db2170bcdf292e2fbe3707ebd103f42c7ea578e3
Zen-Prjkt <zenitsuprjkt@gmail.com>
2022-04-02 22:26:25 +0700

    .

--- download.sh
+++ download.sh
@@ -5,6 +5,6 @@ echo "Downloading few Dependecies . . ."
 # Kernel Sources
 git clone --depth=1 https://github.com/ZenitsuID/android_kernel_xiaomi_lavender -b hmp $CIRRUS_WORKING_DIR/$DEVICE_CODENAME
 # Toolchain 
-git clone --depth=1 https://github.com/sohamxda7/llvm-stable -b gcc64 $CIRRUS_WORKING_DIR/GCC64
-git clone --depth=1 https://github.com/sohamxda7/llvm-stable -b gcc32 $CIRRUS_WORKING_DIR/GCC32
-git clone --depth=1 https://github.com/kdrag0n/proton-clang -b master $CIRRUS_WORKING_DIR/CLANG
+git clone --depth=1 https://github.com/mvaisakh/gcc-arm64.git -b gcc-master $CIRRUS_WORKING_DIR/GCC64
+git clone --depth=1 https://github.com/mvaisakh/gcc-arm.git -b gcc-master $CIRRUS_WORKING_DIR/GCC32
+#git clone --depth=1 https://github.com/kdrag0n/proton-clang -b master $CIRRUS_WORKING_DIR/CLANG
